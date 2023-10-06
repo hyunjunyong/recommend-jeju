306c1943a8186ca5522b54217d3766cca173fde7
현준용 <hjy4649@cocen.com>
2023-10-06 11:02:43 +0900

    kakao type setting

--- pages/recommend/index.vue
+++ pages/recommend/index.vue
@@ -6,6 +6,13 @@
 
 <script setup lang="ts">
 const config = useRuntimeConfig();
+// declare global {
+//   interface Window {
+//     kakao: any;
+//   }
+// }
+
+// kakao 를 글로벌로 선언해 주어야 하므로 아래와 같은 코드가 필요하다.
 
 const map = ref();
 const latitude = ref(37.39843974939604);
@@ -14,11 +21,11 @@ const initMap = () => {
   const container = document.getElementById('map');
 
   const options = {
-    center: new kakao.maps.LatLng(latitude.value, longitude.value),
+    center: new window.kakao.maps.LatLng(latitude.value, longitude.value),
     level: 8,
   };
 
-  map.value = new kakao.maps.Map(container, options);
+  map.value = new window.kakao.maps.Map(container, options);
 };
 onMounted(() => {
   if (!window.kakao || !window.kakao.maps) {
@@ -33,8 +40,8 @@ onMounted(() => {
      * 오류를 내기 때문에 아래 줄과 같이 전역변수임을
      * 알려주어야 한다. */
     /* global kakao */
-    script.addEventListener('load', () => {
-      kakao.maps.load(() => {
+    script.addEventListener('loadka', () => {
+      window.kakao.maps.load(() => {
         // 카카오맵 API가 로딩이 완료된 후 지도의 기본적인 세팅을 시작해야 한다.
         initMap();
       });
